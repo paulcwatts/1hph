0198e5acd275147b08db189b16de87d2575f3e8d
Paul Watts <paulcwatts@gmail.com>
2010-04-11 20:21:24 -0700

    Allow for local settings

--- gonzo/settings.py
+++ gonzo/settings.py
@@ -30,7 +30,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -96,3 +96,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
 )
+
+try:
+    from local_settings import *
+except:
+    pass
