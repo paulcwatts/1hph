f64a7bf41566dd3fc225c96bf702cecee057b3f5
Paul Watts <paulcwatts@gmail.com>
2010-04-11 20:43:23 -0700

    Enable the admin

--- gonzo/urls.py
+++ gonzo/urls.py
@@ -2,20 +2,16 @@ from django.conf.urls.defaults import *
 import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'gonzo.views.home'),
     # Example:
     # (r'^gonzo/', include('gonzo.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
