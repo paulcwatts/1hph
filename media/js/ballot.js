9df9b8f9648b0237e3174fa9ae374163c30f587f
Paul Watts <paulcwatts@gmail.com>
2010-05-18 15:52:19 -0700

    Fix ballot bug
    Rewrite the load logic to work around a hanging problem.

--- media/js/ballot.js
+++ media/js/ballot.js
@@ -10,55 +10,23 @@
         var ballot = $(this);
         var imgcontainer = $(".ballot-img-container", ballot);
         var loading = $(".loading", ballot);
-        var leftimg = $(".ballot-left img", ballot);
-        var rightimg = $(".ballot-right img", ballot);
         var overlays = $(".ballot-overlay", ballot);
+        var ballotleft = $(".ballot-left", imgcontainer);
+        var ballotright = $(".ballot-right", imgcontainer);
 
+        var leftimg = null;
+        var rightimg = null;
         var numLoaded = 0;
         function onImgLoad() {
             ++numLoaded;
             if (numLoaded == 2) {
               // Now we can show the container and hide loading
               loading.hide();
-              // Ensure the winner and loser classes aren't there
-              leftimg.parent().removeClass("winner").removeClass("loser");
-              rightimg.parent().removeClass("winner").removeClass("loser");
+              ballotleft.prepend(leftimg);
+              ballotright.prepend(rightimg);
+
               imgcontainer.fadeIn('fast');
             }
-        }
-        function showBallot(data) {
-            var submissions = data.submissions;
-            var left = submissions[0];
-            var right = submissions[1];
-            numLoaded = 0;
-            leftimg.data("json", left);
-            rightimg.data("json", right);
-            leftimg.attr("src", left.thumbnail_url);
-            rightimg.attr("src", right.thumbnail_url);
-        }
-        function doVote(winner, loser) {
-            var img = $(this);
-            var json = winner.data("json");
-            winner.parent().addClass("winner");
-            loser.parent().addClass("loser");
-            // Wait for a second and then go back to the beginning
-            window.setTimeout(function() {
-              imgcontainer.fadeOut('fast', function() {
-                  $.ajax({
-                      type: 'POST',
-                      url: config.url,
-                      data: { url: json.url },
-                      dataType: 'json',
-                      success: function(data, textStatus, xhr) {
-                        showBallot(data);
-                      },
-                      error: function(xhr, textStatus) {
-                        alert("ERROR");
-                      }
-                    });
-              });
-              loading.show();
-            }, 500);
         }
         function onImgLeftClick() {
           doVote(leftimg, rightimg);
@@ -66,10 +34,54 @@
         function onImgRightClick() {
           doVote(rightimg, leftimg);
         }
+        function showBallot(data) {
+            var submissions = data.submissions;
+            var left = submissions[0];
+            var right = submissions[1];
+            numLoaded = 0;
+            leftimg = $('<img/>')
+              .data("json", left)
+              .addClass("submission")
+              .load(onImgLoad)
+              .click(onImgLeftClick)
+              .attr('src', left.thumbnail_url);
+            rightimg = $('<img/>')
+              .data("json", right)
+              .addClass("submission")
+              .load(onImgLoad)
+              .click(onImgRightClick)
+              .attr('src', right.thumbnail_url);
 
-        // set up onclicks and loads for the images
-        leftimg.click(onImgLeftClick).load(onImgLoad);
-        rightimg.click(onImgRightClick).load(onImgLoad);
+        }
+        function doVote(winner, loser) {
+            var json = winner.data("json");
+            winner.parent().addClass("winner");
+            loser.parent().addClass("loser");
+            // Wait for a second and then go back to the beginning
+            window.setTimeout(function() {
+              imgcontainer.fadeOut('fast', function() {
+                  // Remove the old images
+                var imgs = $(".submission", imgcontainer);
+                imgs.parents()
+                  .removeClass("winner")
+                  .removeClass("loser");
+                imgs.remove();
+                $.ajax({
+                    type: 'POST',
+                    url: config.url,
+                    data: { url: json.url },
+                    dataType: 'json',
+                    success: function(data, textStatus, xhr) {
+                      showBallot(data);
+                    },
+                    error: function(xhr, textStatus) {
+                      //alert("ERROR");
+                    }
+                  });
+                });
+              loading.show();
+           }, 500);
+        }
 
         // show loading
         imgcontainer.hide();
