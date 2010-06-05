e65f8676134137bcf565622565cb15a73802e41e
Paul Watts <paulcwatts@gmail.com>
2010-06-05 11:53:40 -0700

    Random small improvements to the aux pages.

--- gonzo/webapp/help/urls.py
+++ gonzo/webapp/help/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('gonzo.webapp.help.views',
     (r'^$',            direct_to_template, {'template':'help/index.html'}),
     (r'^privacy/$',    direct_to_template, {'template':'help/privacy.html'}),
     (r'^tos/$',        direct_to_template, {'template':'help/tos.html'}),
+    (r'^about/$',      direct_to_template, {'template':'help/about.html'})
 )
