3fb11bbb735f12bc67bd86e67b3f963c8bc1f14b
Paul Watts <paulcwatts@gmail.com>
2010-06-06 13:38:50 -0700

    Run memcached in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
    echo 'No virtualenv, bailing...'
    exit 1
 fi
+memcached &
 rabbitmq-server &
 PYTHONPATH=. GONZO_LOCAL_SETTINGS_MODULE=gonzo.local_settings CELERY_CONFIG_MODULE=gonzo.settings celerybeat &
 gonzo/manage.py runserver
